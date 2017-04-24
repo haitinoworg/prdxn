997415a3562edc007a085a18c0e6eb4e8bfb98be
Pravin Chukkala <pravin.chukkala.prdxn@gmail.com>
2017-04-24 17:56:38 +0530

    Created Custom Single Program Detail Page Template

--- wp-content/themes/parallax-pro/single-programs.php
+++ wp-content/themes/parallax-pro/single-programs.php
@@ -1,66 +1,64 @@
 <?php
-/**
+	/**
  * The custom programs post type archive template
  */
 
-//* Remove the post date, author name
-remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
-remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
-remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+	//* Remove the post date, author name
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+	remove_action( 'genesis_after_header', 'genesis_do_nav' );
+	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+	remove_action( 'genesis_entry_content', 'genesis_do_post_content_nav' );
+// remove_action( 'genesis_entry_content', 'genesis_do_post_content'); 
 
+// * Hero Image with donate content
+	add_action( 'genesis_before_entry', 'programs_single_hero' );
+	function programs_single_hero() {
+		echo '<div class="programs-hero-image"><div class="wrap"><div class="hero-content">';
+		echo '<h3>';
+		echo the_title() .'</h3>';
+		echo '<p>';
+		echo the_field("donate_text") . '</p>';
+		echo do_shortcode('[direct-stripe type="donation" name="My plugin" currency="USD" description="Help me improve the plugin" label="Donate Now" panellabel="This will add one more setting option!"]').'</div></div>';
+		if ( $image = genesis_get_image( 'format=url&size=programs' ) ) {
+			printf( '<img src="%s" alt="%s" />', $image, the_title_attribute( 'echo=0' ) );
+			echo '</div>';
 
-	// * Add the featured image after post title
-add_action( 'genesis_after_entry', 'programs_single_hero' );
-function programs_single_hero() {
-	echo '<div class="one-half first single-programs-featured">';
-	if ( $image = genesis_get_image( 'format=url&size=programs' ) ) {			printf( '<img src="%s" alt="%s" />', $image, the_title_attribute( 'echo=0' ) );
-} else {
-
-	echo '<img src="' . get_bloginfo( 'stylesheet_directory' )
-	. '/images/empty-image.png" />';
-}
-echo '</div><div class="one-half">' . do_shortcode('[direct-stripe type="donation" name="My plugin" currency="USD" description="Help me improve the plugin" label="Donate Now" panellabel="This will add one more setting option!"]').'</div><div class="clearfix"></div>';
-
-}
-
-
-
-add_action( 'genesis_entry_footer', 'programs_custom_video' );
-function programs_custom_video() {
-	?>
-	<div class="lightbox-video">
-		<?php
-
-		if ( $value1 = get_field( "video1" ) ) {
-			echo '<div>'. wp_oembed_get($value1) . '</div>';
-		}
-		if( $value2 = get_field("video2")) {
-			echo '<div>'. wp_oembed_get($value2) . '</div>';
 		}
 
-		?>
-	</div>
-	<?php
-}
+	}
+
+
+
+	// * Post List Content
+	add_action('genesis_after_entry','custom_list_content');
+	function custom_list_content() {
+		$list_content = get_field('list-content');
+		if($list_content) {
+			echo $list_content;
+		} else { echo '';	}
+	}
+
 
 // * Removing Comment Section
-remove_action( 'genesis_after_post', 'genesis_get_comments_template' );
+	remove_action( 'genesis_after_post', 'genesis_get_comments_template' );
 
 // Removing Genesis Before Loop and Footer Default Copy Right Section
-remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
-remove_action( 'genesis_footer', 'genesis_do_footer' );
+	remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+	remove_action( 'genesis_footer', 'genesis_do_footer' );
 
-remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+	remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+	remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+	remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
 
 //* Remove the entry meta in the entry footer
-remove_action( 'genesis_entry_content', 'genesis_do_post_content_nav', 12 );
+	remove_action( 'genesis_entry_content', 'genesis_do_post_content_nav', 12 );
 
 
 //* Removes only the comment form
-remove_action( 'genesis_comment_form', 'genesis_do_comment_form' );
+	remove_action( 'genesis_comment_form', 'genesis_do_comment_form' );
 
-genesis();
+	genesis();
 
-?>
+	?>
