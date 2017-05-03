68831b1418cb2943fba16db00c6be30c5cf5cc94
sumit tiwari <sumit.tiwari.prdxn@gmail.com>
2017-05-03 13:09:30 +0530

    redesigned footer, homepage program section and banner content gutter spaced changed

--- wp-content/plugins/genesis-grandchild/grandchild-scripts.js
+++ wp-content/plugins/genesis-grandchild/grandchild-scripts.js
@@ -130,5 +130,14 @@ $(this).scroll(function(){
     }
 });
 
+
+
 })(jQuery);
 
+ (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
