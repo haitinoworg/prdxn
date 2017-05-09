172d24bc42c145a8ec3edb18bbfc221cce3b49de
Pravin Chukkala <pravin.chukkala.prdxn@gmail.com>
2017-05-09 12:39:18 +0530

    Books Load More Logic Implemented

--- wp-content/themes/parallax-pro/template-parts/page/three-columns.php
+++ wp-content/themes/parallax-pro/template-parts/page/three-columns.php
@@ -11,8 +11,6 @@
 global $post;
 $post_slug=$post->post_name;
 
-echo do_shortcode('[ajax_load_more container_type="div" post_type="post" posts_per_page="2" category="$post_slug" scroll="false" images_loaded="false" button_label="View More" button_loading_label="load-more-btn"]');
 
-
-
+echo do_shortcode('[ajax_load_more container_type="div" post_type="post" posts_per_page="3" category="'. $post_slug .'" scroll="false" button_label="View More" button_loading_label="Loading..." transition_container="false"]');
 
